8521df69e4640fde9116c6f291e5d70a94225ca9
Oleg Lavrovsky <oleg@utou.ch>
2013-09-17 23:43:00 +0200

    Reformatted without jQuery UI

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -3,29 +3,13 @@ javascript:(function() {
     var v = "1.10.2";
 
     if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-        var done = false;
-        var secondDone = false;
+        var done = false
         var script = document.createElement("script");
         script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
         script.onload = script.onreadystatechange = function() {
             if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
                 done = true;
-
-                var css = document.createElement("link");
-                css.rel = "stylesheet";
-                css.type = "text/css";
-                css.href = "http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css";
-                document.getElementsByTagName("head")[0].appendChild(css);
-
-                var uiScript = document.createElement("script");
-                uiScript.src = "http://code.jquery.com/ui/1.10.3/jquery-ui.js";
-                uiScript.onload = uiScript.onreadystatechange = function() {
-                    if (!secondDone && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-                        secondDone = true;
-                        initMyBookmarklet();
-                    }
-                };
-                document.getElementsByTagName("head")[0].appendChild(uiScript);
+                initMyBookmarklet();
             }
         };
         document.getElementsByTagName("head")[0].appendChild(script);
@@ -37,49 +21,47 @@ javascript:(function() {
         (window.myBookmarklet = function() {
 
             $("body").append("\
-         <div id=\"dialog-form\" title=\"Open this data!\">\
-         <form>\
+         <div id=\"odb-bkmlt-dialog-form\" style=\"display:none\">\
+         <form><h5>Open this data!</h5>\
          <fieldset>\
          <label for=\"title\">Title</label>\
-         <input type=\"text\" name=\"title\" id=\"title\" class=\"text ui-widget-content ui-corner-all\"/>\
+            <input type=\"text\" name=\"title\" id=\"title\"/><br/>\
          <label for=\"context\">Context</label>\
          <select name=\"context\" id=\"context\">\
-         <option value=\"Academic\">Academic</option>\
-         <option value=\"Commercial\">Commercial</option>\
-         <option value=\"Government\">Government</option>\
-         </select>\
+             <option value=\"private\">Private</option>\
+             <option value=\"academic\">Academic</option>\
+             <option value=\"commercial\">Commercial</option>\
+             <option value=\"government\">Government</option>\
+         </select><br/>\
          <label for=\"reason\">Reason</label>\
-         <input type=\"radio\" name=\"reason\" value=\"legal\"/> : Legal<br/>\
-         <input type=\"radio\" name=\"reason\" value=\"technical\"/> : Technical<br/>\
-         </fieldset>\
+             <input type=\"checkbox\" checked name=\"reason\" value=\"legal\">Legal</button>\
+             <input type=\"checkbox\" name=\"reason\" value=\"technical\">Technical</button>\
+         </fieldset><br/>\
+         <button class=\"submit\">Submit</button>\
+         <button class=\"cancel\">Cancel</button>\
          </form>\
          </div>\
+         <style type=\"text/css\">\
+         #odb-bkmlt-dialog-form { width:30em; z-index:9999; font-size:16px; position:fixed; top:0px; left:5em; padding:2em; border:1px solid black; box-shadow:5px 5px 10px #888; background: white; }\
+         #odb-bkmlt-dialog-form h5 { font-size:24px; } #odb-bkmlt-dialog-form label { width:6em; display:inline-block; }\
+         </style>\
          ");
+            
+            var dialog = $('#odb-bkmlt-dialog-form').fadeIn(); 
+            $('#title', dialog).val(document.title);
 
-            $("#title").val(document.title);
-
-            $("#dialog-form").dialog({
-                autoOpen: false,
-                height: 300,
-                width: 350,
-                modal: true,
-                buttons: {
-                    "Submit": function() {
-                        console.log(document.title);
-                        console.log($("#title").val());
-                        console.log($("#context").val());
-                        console.log($("input[name=\"reason\"]:checked").val());
-
-                    },
-                    Cancel: function() {
-                        $(this).dialog("close");
-                    }
-                },
-                close: function() {
-                }
+            $('button.submit', dialog).click(function() {
+                console.log(document.title);
+                console.log($("#title").val());
+                console.log($("#context").val());
+                console.log($("input[name=\"reason\"]:checked").val());
+                dialog.fadeOut();
+                return false;
             });
 
-            $("#dialog-form").dialog("open");
+            $('button.cancel', dialog).click(function() {
+                dialog.remove();
+            });
 
         })();
     }
