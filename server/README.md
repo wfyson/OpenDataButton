9bca1bd8e58603047f33d773e1f11f7918a31d15
Oleg Lavrovsky <oleg@utou.ch>
2013-09-17 22:56:46 +0200

    Added server to main project

---
+++ server/README.md
@@ -0,0 +1,14 @@
+ODB Server
+==========
+
+A RESTful API for the Open Data Button.
+
+## Developer install
+
+1. Install virtualenv and create one in this folder $ virtualenv .
+2. Activate the instance $ source bin/activate
+3. Install requirements $ pip -r requirements.txt
+4. Start the server $ python manage.py runserver
+5. Start a local redis server
+6. Open the home page http://localhost:5000
+
---
+++ server/opendatabutton/__init__.py
@@ -0,0 +1,83 @@
+import os, uuid
+from flask import Flask, render_template, send_from_directory, request, json, jsonify
+
+if 'REDISTOGO_URL' in os.environ:
+	from werkzeug.contrib.cache import RedisCache
+	REDISTOGO_PASS = os.environ['REDISTOGO_PASS']
+	cache = RedisCache(host='cod.redistogo.com', port=9747, password=REDISTOGO_PASS, default_timeout=300, key_prefix=None)
+	cache_type = "redistogo"
+elif 'REDISLOCAL' in os.environ:
+	from werkzeug.contrib.cache import RedisCache
+	cache = RedisCache(host='localhost', port=16379, key_prefix='kh_')
+	cache_type = "redisunix"
+else:
+	from werkzeug.contrib.cache import SimpleCache
+	cache = SimpleCache()
+	cache_type = "local"
+
+app = Flask(__name__)
+
+# Home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Home page
+@app.route('/api')
+def api():
+    return render_template('api.html')
+
+# Favicon
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+# Submission API
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+	if request.headers['Content-Type'] == 'application/json':
+		jsondata = request.json
+	else:
+		jsondata = {
+			"url": 		request.form['url'],
+			"title": 	request.form['title'],
+			"context": 	int(request.form['context']),
+			"reason": 	request.form['reason'],
+			"lon": 		request.form['lon'],
+			"lat": 		request.form['lat']
+		}
+
+	# Consistent encoding
+	try:
+		if jsondata['url']:   jsondata['url'] = u'' + jsondata['url'].decode('utf-8').strip()
+		if jsondata['title']: jsondata['title'] = u'' + jsondata['title'].decode('utf-8').strip()
+		print "[info] adding %s|%s" % (jsondata['url'], jsondata['title'])
+	except UnicodeEncodeError:
+		if jsondata['url']:   jsondata['url'] = u'' + unicode(jsondata['url']).strip()
+		if jsondata['title']: jsondata['title'] = u'' + unicode(jsondata['title']).strip()
+		print "[info] encoding %s|%s" % (repr(jsondata['url']), repr(jsondata['title']))
+	
+	# Validation
+	if jsondata['url'] and len(jsondata['url']) < 2: jsondata['url'] = None
+	if jsondata['title'] and len(jsondata['title']) < 2: jsondata['title'] = None	
+	if not (jsondata['url'] and jsondata['title']):
+		return json.dumps({ 'ErrorCode': 401, 'Message': "Parameters missing" })
+
+	# Save the submission
+	hash_key = str(uuid.uuid1())
+
+	rv = cache.get(jsondata['url'])
+	if rv is not None:
+		rv.append(hash_key)
+	else:
+		rv = [hash_key]
+	cache.set(jsondata['url'], rv, timeout=999999999999999999)
+	cache.set(hash_key, jsondata, timeout=999999999999999999)
+	jsondata['rv'] = rv
+	return json.dumps(jsondata)
+
+# Standard app construct
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
