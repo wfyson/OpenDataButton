9ec1ceb312e759ad0aae8ee44d5ce121bb90e306
Oleg Lavrovsky <oleg@utou.ch>
2013-09-19 00:24:44 +0200

    DB schema

---
+++ server/schema.sql
@@ -0,0 +1,11 @@
+drop table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+  time int not null,
+  title text not null,
+  url text not null,
+  context text not null,
+  reason text not null,
+  lon real,
+  lat real
+);
